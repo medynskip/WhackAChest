8d1751258488a5e84da7194ffa88e1941f74da43
Piotr Medynski <medynski@outlook.com>
2020-02-08 19:00:31 +0100

    timer added counting 30 seconds

--- js/app.js
+++ js/app.js
@@ -1,44 +1,19 @@
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const scoreField = document.querySelector("#score");
+const timer = document.querySelector(".timer");
 const allBoxes = document.querySelectorAll(".bracket");
 const difficulty = [
     [2000, 600],
     [1000, 400]
 ];
-// rezerwacja zmiennej na interwal
+// rezerwacja zmiennej na interwaly i timeouty
 let mainLoop = "";
 let endGame = "";
+let seconds = "";
 // rezerwacja zmiennej na poziom trudnosci
 let selectedDif = 0;
 let score = 0;
-
-for (let i = 0; i < allBoxes.length; i++) {
-    allBoxes[i].addEventListener("click", function () {
-        if (this.classList.contains("active")) {
-            this.classList.add("coin");
-            setTimeout(function () {
-                allBoxes[i].classList.remove("coin");
-            }, 500);
-            score += 1;
-            scoreField.innerText = score;
-            this.classList.remove("active");
-        }
-    });
-
-    allBoxes[i].addEventListener("mouseover", function () {
-        if (!this.classList.contains("active")) {
-            this.classList.add("teasing");
-        }
-    });
-    allBoxes[i].addEventListener("mouseout", function () {
-        if (this.classList.contains("teasing")) {
-            this.classList.remove("teasing");
-        }
-    });
-}
-
-
 
 function randNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -54,11 +29,26 @@ function mainFunction() {
     }, difficulty[selectedDif][0]);
 }
 
+function counter() {
+    const start = (new Date()).getTime();
+    seconds = setInterval(function () {
+        let now = (new Date()).getTime();
+        let left = 30 - (Math.floor((now - start) / 1000));
+        (left > 10) ? timer.innerText = left: timer.innerText = '0' + left;
+        if (left < 0) {
+            clearInterval(seconds);
+            timer.innerText = '30';
+        }
+    }, 1000)
+}
+
 startBtn.addEventListener("click", function () {
     startBtn.disabled = true;
+    stopBtn.disabled = false;
     score = 0;
     scoreField.innerText = score;
     selectedDif = document.querySelector("#difficulty").value;
+    counter();
     mainLoop = setInterval(mainFunction, difficulty[selectedDif][1]);
     endGame = setTimeout(function () {
         clearInterval(mainLoop);
@@ -69,23 +59,28 @@ startBtn.addEventListener("click", function () {
 
 stopBtn.addEventListener("click", function () {
     clearInterval(mainLoop);
+    clearInterval(seconds);
     clearTimeout(endGame);
     startBtn.disabled = false;
+    stopBtn.disabled = true;
 });
 
+for (let i = 0; i < allBoxes.length; i++) {
+    allBoxes[i].addEventListener("click", function () {
+        if (this.classList.contains("active")) {
+            this.classList.add("coin");
+            setTimeout(function () {
+                allBoxes[i].classList.remove("coin");
+            }, 500);
+            score += 1;
+            scoreField.innerText = score;
+            this.classList.remove("active");
+        }
+    });
+}
 
 var inactivity = setInterval(function () {
     if (!startBtn.disabled) {
-        // for (let i = 0; i < allBoxes.length; i++) {
-
-        //     setTimeout(function () {
-        //         allBoxes[i].classList.add("teasing");
-        //     }, 80 * i)
-        //     setTimeout(function () {
-        //         allBoxes[i].classList.remove("teasing");
-        //     }, (80 * i) + 1000)
-        // }
-
         let x = randNum(1, 25);
         allBoxes[x].classList.add("teasing");
         setTimeout(function () {
